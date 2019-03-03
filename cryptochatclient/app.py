611039f7fd28d3c861497959ec39c7f31375b237
Šárka Chwastková <chwastkova.s@gmail.com>
2019-03-03 21:50:43 +0100

    Edited GUI for the app.

--- cryptochatclient/app.py
+++ cryptochatclient/app.py
@@ -1,58 +1,33 @@
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
+import os
 
-def run():
-    window = CryptoChat()
-    window.connect("destroy", Gtk.main_quit)
-    window.show_all()
-    Gtk.main()
-
-class CryptoChat(Gtk.Window):
+class CryptoChat:
     def __init__(self):
-        Gtk.Window.__init__(self, title="CryptoChat")
-        self.set_size_request(800, 650)
-        hbox = Gtk.Box(spacing=10)
-        hbox.set_homogeneous(False)
-        vbox_left = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-        vbox_left.set_homogeneous(False)
-        vbox_right = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-        vbox_right.set_homogeneous(False)
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file('./cryptochatclient/resources/ui/visual.glade')
+        self.builder.connect_signals(self)
         
-        #self.horizontalPane = Gtk.HPaned()
-        self.verticalPane = Gtk.VPaned()
-        paneBox1 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-        paneBox1.set_homogeneous(False)
-
-        self.textBox = Gtk.Entry()
-        self.btnSend = Gtk.Button(label="Send message")
-        self.btnSend.connect("clicked", self.btn_send)
-
-        self.sidebar = Gtk.Frame(label="Contacts")
-        self.sidebar.set_label_align(0.05, 0.5)
-
-        self.chatArea = Gtk.Frame(label="Chat area")
-        self.chatArea.set_label_align(0.05, 0.5)
+        self.window = self.builder.get_object('window1')
+        self.window.show_all()
+        Gtk.main()
         
-        #self.mainBox.pack_start(self.horizontalPane, True, True, 0)
-        #self.horizontalPane.pack1(self.sidebar, True, True)
-        #self.horizontalPane.pack2(self.verticalPane, True, True)
-        paneBox1.pack_start(self.textBox, True, True, 0)
-        paneBox1.pack_start(self.btnSend, True, True, 0)
+    def onDestroy(self):
+        Gtk.main_quit()
+    
+    def onTextViewSet(self, textView, inputMessage):
+        self.textview = self.builder.get_object("chatWindow")
+        textbuffer = self.textview.get_buffer() 
+        end_iter = textbuffer.get_end_iter()
+        if inputMessage:
+            textbuffer.insert(end_iter, '\n\nMe:\n' + inputMessage)
 
-        self.verticalPane.pack1(self.chatArea, True, True)
-        self.verticalPane.pack2(paneBox1, True, True)
-
-        hbox.pack_start(vbox_left, True, True, 0)
-        hbox.pack_start(vbox_right, True, True, 0)
-        
-        vbox_left.pack_start(self.sidebar, True, True, 0)
-        vbox_right.pack_start(self.verticalPane, True, True, 0)
-
-        self.add(hbox)
-
-    def btn_clicked(self, widget):
-        self.lbl.set_markup("<span foreground='#599128' size='x-large'>Here!</span>")
-
-    def btn_send(self, widget):
-        print ('Message was sent.')
+    def onButtonPressed(self, button):
+        # TODO: send message to DB (server)
+        inputMessage = self.builder.get_object("message").get_text()
+        self.onTextViewSet(self, inputMessage)
+        self.builder.get_object("message").set_text('')
+    
+def run():
+    app = CryptoChat()
